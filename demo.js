e13bdc214482a8ad9457e34bf29a2dae62700669
Peck, Marcin <Marcin.Peck@finastra.com>
2020-09-17 15:17:04 +0200

    js objects and properties

--- demo.js
+++ demo.js
@@ -1,6 +1,42 @@
-'use strict'; 
-(function() {
+'use strict';
+(function () {
 
-  display('Hello World');
+
+    let person = {
+        firstName: 'Marcin',
+        lastName: 'Peck',
+        age: 30
+    };
+
+    display(person.firstName);
+
+    //Object.defineProperty(person,'firstName', {writable: false});
+    //Object.freeze(person)
+    // person.firstName = 'Ala'; // fail
+
+   //Object.defineProperty(person, 'firstName', {configurable: false});
+  // Object.defineProperty(person,'firstName', {enumerable: false});  // fail
+
+  Object.defineProperty(person, 'fullName',
+      {
+        get() {
+          return this.firstName + ' ' + this.lastName
+        },
+        set(v) {
+          let nameParts = v.split(' ');
+          this.firstName = nameParts[0];
+          this.lastName = nameParts[1];
+        }
+      });
+
+  display(person.fullName);
+  person.fullName = "Ala Kotecka";
+  display(person.fullName)
+
+
+  //this will not work if configurable set to false
+  delete person.firstName;
+
+  display(person)
 
 })();
